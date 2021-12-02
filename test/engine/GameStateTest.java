ef1f2ff029beed98fb0ddaa9500a0a076400afc4
ChangSun Park <changsun19991212@gmail.com>
2021-12-03 02:23:32 +0900

    gradle init

--- test/engine/GameStateTest.java
+++ test/engine/GameStateTest.java
@@ -1,4 +1,12 @@
+package engine;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GameStateTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void getLevel() {
+        GameState gs = new GameState(1,0,3,0,0);
+        assertEquals(gs.getLevel(), 1);
+    }
 }
